26a2973407a9efd5c96e1a4aa3b8331f642ad929
aortiz <aoa2eso@gmail.com>
2024-05-20 12:48:25 +0200

    Inicialización de logs en main

---
+++ main.py
@@ -0,0 +1,9 @@
+import config
+import logging
+
+logger = logging.getLogger("Contrataciones")
+logger.setLevel(logging.INFO)
+
+if __name__ == '__main__':
+    logger.info("Inicio de Ejecución")
+    logger.info("Fin de Ejecución")
