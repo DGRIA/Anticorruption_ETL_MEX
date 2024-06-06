b9567397a85e0afc61453d55702029d40c85eee2
aortiz <aoa2eso@gmail.com>
2024-06-06 05:18:11 +0200

    Eliminacion de comentarios y TODO de mejoras a implementar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import config
 from config import *
-from src import data_download_unzip
+from src.data_download_unzip import download_contrataciones_zip, unzip
 from src.mongodb_utils import connect_to_mongodb, process_large_json
 import time
 import streamlit as st
@@ -10,6 +10,7 @@ import config
 import sys
 import os
 import shutil
+
 
 def show_intro():
     st.markdown((
@@ -24,8 +25,11 @@ def show_intro():
     st.markdown("<br>", unsafe_allow_html=True)
     cols = st.columns([1, 1, 1])  # Create three columns
     inner_cols = cols[2].columns([1, 1, 1, 1])  # Create two columns inside the middle column
-    inner_cols[0].markdown("<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with love</p>", unsafe_allow_html=True) # Center the text, change the font, and add padding
-    inner_cols[2].image('docs/images/mottum2.png', use_column_width=True) 
+    inner_cols[0].markdown(
+        "<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with "
+        "love</p>",
+        unsafe_allow_html=True)  # Center the text, change the font, and add padding
+    inner_cols[2].image('docs/images/mottum2.png', use_column_width=True)
 
 
 def start_process():
@@ -34,29 +38,37 @@ def start_process():
                              key='start_process_button'):
         st.session_state.button_pressed = True
         st.session_state.main_running = True
-        #main()
+        # main()
 
     st.markdown("<br>", unsafe_allow_html=True)
     cols = st.columns([1, 1, 1])  # Create three columns
     inner_cols = cols[2].columns([1, 1, 1, 1])  # Create two columns inside the middle column
-    inner_cols[0].markdown("<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with love</p>", unsafe_allow_html=True) # Center the text, change the font, and add padding
-    inner_cols[2].image('docs/images/mottum2.png', use_column_width=True) 
+    inner_cols[0].markdown(
+        "<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with "
+        "love</p>",
+        unsafe_allow_html=True)  # Center the text, change the font, and add padding
+    inner_cols[2].image('docs/images/mottum2.png', use_column_width=True)
 
+
+# TODO ADAPTAR A CONTRATACIONES
+# USAR LAS RUTAS PUESTAS EN CONFIG
 def show_finished():
-    if os.path.exists("data/merged_dataset.csv"):
+    if os.path.exists("data/merged_dataset.csv_files"):
         st.markdown("<h2 style='text-align: center;'>¡El dataset está listo!</h2>", unsafe_allow_html=True)
         cols = st.columns([1, 2, 1])
-        with open("data/merged_dataset.csv", "rb") as file:
+        with open("data/merged_dataset.csv_files", "rb") as file:
             button_clicked = cols[1].download_button(
                 label="Pulsa aquí para descargar el dataset completo.",
                 data=file,
-                file_name="merged_dataset.csv",
-                mime="text/csv",
+                file_name="merged_dataset.csv_files",
+                mime="text/csv_files",
             )
         st.markdown("<br>", unsafe_allow_html=True)
         cols = st.columns([1, 1, 1])  # Create three columns
         inner_cols = cols[2].columns([1, 1, 1, 1])  # Create two columns inside the middle column
-        inner_cols[0].markdown("<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with love</p>", unsafe_allow_html=True) # Center the text, change the font, and add padding
+        inner_cols[0].markdown(
+            "<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with love</p>",
+            unsafe_allow_html=True)  # Center the text, change the font, and add padding
         inner_cols[2].image('docs/images/mottum2.png', use_column_width=True)  # Colocar la im
 
     else:
@@ -67,8 +79,11 @@ def show_finished():
 
         cols = st.columns([1, 1, 1])  # Create three columns
         inner_cols = cols[2].columns([1, 1, 1, 1])  # Create two columns inside the middle column
-        inner_cols[0].markdown("<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with love</p>", unsafe_allow_html=True) # Center the text, change the font, and add padding
+        inner_cols[0].markdown(
+            "<p style='text-align: center; font-family: Comic Sans MS; padding-top: 12px; white-space: nowrap;'>Made with love</p>",
+            unsafe_allow_html=True)  # Center the text, change the font, and add padding
         inner_cols[2].image('docs/images/mottum2.png', use_column_width=True)
+
 
 def clear_directory(directory):
     for filename in os.listdir(directory):
@@ -81,18 +96,23 @@ def clear_directory(directory):
         except Exception as e:
             print(f'Failed to delete {file_path}. Reason: {e}')
 
+
 logger = logging.getLogger("Contrataciones")
 logger.setLevel(logging.INFO)
 
-def main():
-    if not os.path.exists('data'):
-            os.makedirs('data')
-            print("Directory 'data' missing, creating data directory.")
-    if not os.path.exists('data/Raw'):
-        os.makedirs('data/productores_autorizados')
-        print("Directory 'data/productores_autorizados' missing, creating data/productores_autorizados.")
 
-    clear_directory('data/Raw')
+# TODO NO ES NECESARIO CREAR LOS DIRECTORIOS
+# TODO USAR LAS RUTAS DE CONFIG
+def main():
+    if not os.path.exists(path_config.data_path):
+        os.makedirs(path_config.data_path)
+        logger.info("Directory 'data' missing, creating 'data' directory.")
+    if not os.path.exists(path_config.contrataciones_raw_unzip_path):
+        os.makedirs(path_config.contrataciones_raw_unzip_path)
+        logger.info("Directory %s missing, creating %s", path_config.contrataciones_raw_unzip_path,
+                    path_config.contrataciones_raw_unzip_path)
+
+    clear_directory(config.path_config.contrataciones_raw_unzip_path)
 
     with st.spinner(
             'Ejecutando scripts... Esto puede tardar unos minutos. No cambie de pestaña hasta que el proceso haya acabado!'):
@@ -106,6 +126,7 @@ def main():
             if result.returncode != 0:
                 logger.error(f"{script} failed with error:\n{result.stderr}")
                 break
+
 
 if __name__ == '__main__':
     st.markdown('''
@@ -140,7 +161,8 @@ if __name__ == '__main__':
         st.session_state.page = '1. Introducción'
 
     # Create navigation menu
-    st.session_state.page = st.radio('Process', ['1. Introducción', '2. Descarga y unzip', '3. Extracción de datos de MongoDB'])
+    st.session_state.page = st.radio('Process',
+                                     ['1. Introducción', '2. Descarga y unzip', '3. Extracción de datos de MongoDB'])
 
     # Display the selected page
     if st.session_state.page == '1. Introducción':
@@ -151,17 +173,17 @@ if __name__ == '__main__':
         show_finished()
 
     st.markdown(
-    """
-    <style>
-        [data-testid=stSidebar] [data-testid=stImage]{
-            text-align: center;
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-            width: 100%;
-        }
-    </style>
-    """, unsafe_allow_html=True
+        """
+        <style>
+            [data-testid=stSidebar] [data-testid=stImage]{
+                text-align: center;
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 100%;
+            }
+        </style>
+        """, unsafe_allow_html=True
     )
 
     st.sidebar.markdown("<br>", unsafe_allow_html=True)
@@ -175,7 +197,7 @@ if __name__ == '__main__':
     st.sidebar.markdown(
         """
     - [Link al repositorio](https://github.com/MottumData/Anticorruption_ETL_MEX)
-    - [Jupyter](http://localhost:8888/lab) need to change to the correct link.
+    - [Jupyter](http://localhost:8889/lab)
     """
     )
 
@@ -185,10 +207,9 @@ if __name__ == '__main__':
         '**Financiado por:**',
         unsafe_allow_html=True
     )
-    cols = st.columns([1, 1, 1]) 
-    
-    st.sidebar.image('docs/images/UNDP2.png', width=100)
+    cols = st.columns([1, 1, 1])
 
+    st.sidebar.image('docs/images/UNDP2.png', width=100)
 
     if 'button_pressed' not in st.session_state:
         st.session_state.button_pressed = False
@@ -196,43 +217,3 @@ if __name__ == '__main__':
     st.markdown("<br>", unsafe_allow_html=True)
 
 # image_placeholder.image('docs/images/mottum.svg', width=500)
-'''
-De cara a usar el codigo en Streamlit habria que trabajar en preguntar al usuario si desea 
-descargar de nuevo el json, si lo desea extraer, generar csv, parquet, etc.
-'''
-
-''' 
-    1. Descarga de datos
-    2. Extracción de datos
-    3. Insertar datos en MongoDB
-    4. Exportar datos a CSV
-    5. Exportar datos a Parquet
-    
-    Para levantar contenedor de MongoDB (y no tener que instalarlo en la máquina local, aunque puedes instalarlo):
-    docker run --name mongo -p 27017:27017 -d mongo:latest
-    
-    Para levantar compose con MongoDB y Jupyter:
-    docker-compose up -d --build
-    
-    # Para ejecutar Streamlit modificar el archivo Dockerfile y ponerlo igual que Fertilizantes (Levanta Streamlit y Jupyter)
-    
-    
-    logger.info("Inicio de Ejecución")
-    start = time.time()
-    db = connect_to_mongodb()
-    print("Conectado a MongoDB")
-    # data_download_unzip.download_contrataciones_zip() # Tarda unos minutos
-    logger.info("Tiempo de descarga: %s", time.time() - start)
-    start_zip = time.time()
-    # data_download_unzip.unzip() # Tarda unos minutos
-    logger.info("Tiempo de extracción: %s", time.time() - start_zip)
-    end = time.time()
-
-    db = connect_to_mongodb()
-    print("Conectado a MongoDB")
-    print("Revisado la colección")
-    process_large_json(
-        config.path_config.contrataciones_raw_unzip_path + CONTRATACIONES_JSON)  # Tarda mas de 20 minutos
-    logger.info(f"Tiempo de ejecución: {end - start}")
-    logger.info("Fin de Ejecución")
-'''
