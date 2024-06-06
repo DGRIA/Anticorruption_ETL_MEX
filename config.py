eb722c4099fb087cbb85131f20a50bb23f4db928
aortiz <aoa2eso@gmail.com>
2024-06-07 01:12:41 +0200

    Seleccionada por defecto colección de sample

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ class PathConfig:
 # Definición de constantes
 # MongoDB
 DB_NAME = 'Contratos_EDCA'
-COLLECTION_NAME = 'Contratos_ECDA_Bulk' # Cambiar a Contratos_ECDA_sample para probar con un archivo de menor tamaño
+COLLECTION_NAME = 'Contratos_ECDA_sample' # Cambiar a Contratos_ECDA_sample para probar con un archivo de menor tamaño
 DB_URL_DOCKER = 'mongodb://db:27017'
 DB_URL = 'mongodb://localhost:27017'
 # JSON Files
