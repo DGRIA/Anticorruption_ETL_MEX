82919fd5a118a5c242d69e1ac0dc7f8ed8965f3d
aortiz <aoa2eso@gmail.com>
2024-06-12 18:34:25 +0200

    Cambios y mejoras menores

--- src/mongodb_utils.py
+++ src/mongodb_utils.py
@@ -1,6 +1,6 @@
 import logging
 from decimal import Decimal
-
+import os
 import ijson
 from bson import Decimal128
 from pymongo import MongoClient
@@ -56,21 +56,39 @@ def insert_data_to_mongodb(data):
         collection.insert_one(data)
 
 
-def process_large_json(file_path, max_lines=5000):
+def process_large_json(file_path, max_lines=5000, progress_bar=None):
     client = MongoClient(DB_URL)
     db = client[DB_NAME]
     collection = db[COLLECTION_NAME]
     logger.info(f"Inserting data into collection '{COLLECTION_NAME}' in database '{DB_NAME}'...")
 
     with open(file_path, 'rb') as file:
+        # total_items = sum(1 for _ in ijson.items(file, 'item'))
         objects = ijson.items(file, 'item')
-        objects = tqdm(objects, desc="Processing JSON file", unit=" record", dynamic_ncols=True, leave=True,
-                       smoothing=0.05)
+        # objects = tqdm(objects, desc="Processing JSON file", unit=" record", dynamic_ncols=True, leave=True,
+        #                smoothing=0.05)
         chunk = []
         for i, obj in enumerate(objects):
             chunk.append(obj)
             if (i + 1) % max_lines == 0:
                 insert_data_to_mongodb(chunk)
                 chunk = []
+                if progress_bar:
+                    progress_bar.progress(i / 137000, f"Processing JSON file ({i} records)")
         if chunk:  # insert remaining records in the chunk
             insert_data_to_mongodb(chunk)
+    progress_bar.progress(1)
+
+
+def get_file_size(file_path):
+    size_in_bytes = os.path.getsize(file_path)
+    size_in_gb = size_in_bytes / 1073741824  # Convert bytes to GB
+    return round(size_in_gb, 2)
+
+
+def get_collection_count():
+    client = MongoClient(DB_URL)
+    db = client[DB_NAME]
+    collection = db[COLLECTION_NAME]
+    count = collection.count_documents({})
+    return count
